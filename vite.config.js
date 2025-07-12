6d6b406a54a038927eb174d4dee4dfea67aca704
Rash1411 <mishrakrahul2003@gmail.com>
2025-07-12 17:23:47 +0530

    deleted

--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-    proxy: {
-      '/api': {
-        target: 'http://3.110.46.79:5000',
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-        headers: {
-          'Access-Control-Allow-Origin': '*'
-        }
-      }
-    }
-  },
-  build: {
-    outDir: 'dist',
-    sourcemap: true,
-    rollupOptions: {
-      output: {
-        manualChunks: undefined
-      }
-    }
-  },
-  preview: {
-    port: 3000
-  }
 })
